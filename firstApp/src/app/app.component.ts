671404819cf972aebe843a9a6fc4bb55dc83d0e6
luyosamson <luyosamson@gmail.com>
2025-01-22 06:06:59 +0300

    1/22/2025 Introduction to Angular

--- firstApp/src/app/app.component.ts
+++ firstApp/src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  standalone:true,
+  imports: [RouterOutlet,HomeComponent,HeaderComponent],
+  template:`
+    <app-header/>
+   <main> <app-home/></main>
+  `,
   styleUrl: './app.component.css'
+
 })
 export class AppComponent {
   title = 'firstApp';
