c54183c13fd40db278507fd57373bc95f33fea78
luyosamson <luyosamson@gmail.com>
2026-05-02 11:02:47 +0300

    Deleted

--- counter-app/src/app/app.component.ts
+++ counter-app/src/app/app.component.ts
@@ -1,12 +1,36 @@
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'counter-app';
+// }
+
+
+
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports:[CounterComponent]
 })
 export class AppComponent {
-  title = 'counter-app';
+  lastCount: number = 10;
+
+  onCountChanged(newCount: number) {
+    this.lastCount = newCount;
+    console.log('Parent received:', newCount);
+  }
 }
