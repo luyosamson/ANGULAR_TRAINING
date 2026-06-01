910949d2c41c853fc17651bc151f8e3265c75476
luyosamson <luyosamson@gmail.com>
2026-06-01 19:02:13 +0300

    First June

--- counter-app/src/app/app.component.ts
+++ counter-app/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from './counter/counter.component';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
